6569676c33379fcf152b6e9891c5fb62e38d80ef
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-08-28 17:14:37 +0530

    Static keyword

---
+++ Maersk.Practice.CSharp.Console/KeyStatic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basics
+{
+    static class Tutorial //Static Class
+    {
+        public static string Topic = "Static keyword";//Staic variable
+        public static void display() //static method
+        {
+            Console.WriteLine("Today I studied about "+Topic+" topic.");
+        }
+        static Tutorial() //satic constructor
+        {
+            Console.WriteLine("Static constructor is called");
+        }
+    }
+    
+
+    public class KeyStatic
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Topic name is : "+Tutorial.Topic);
+            Tutorial.display();//Calling static method 
+        }
+    }
+}
