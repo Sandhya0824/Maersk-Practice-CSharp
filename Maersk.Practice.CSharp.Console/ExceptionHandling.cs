36441fca0840a19168c473a64f1b9753d830e18d
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-04 01:07:24 +0530

    Exception Handling

---
+++ Maersk.Practice.CSharp.Console/ExceptionHandling.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basics
+{
+    public class ExceptionHandling
+    {
+        public static void Main(string[] args)
+        {
+            //Finally example if exception is handled
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int x = a / b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block is executed");
+            }
+            Console.WriteLine("Rest of the code");
+
+            //Finally example if exception is not handled
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int x = a / b;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block is executed");
+            }
+            Console.WriteLine("Rest of the code");
+        }
+    }
+}
