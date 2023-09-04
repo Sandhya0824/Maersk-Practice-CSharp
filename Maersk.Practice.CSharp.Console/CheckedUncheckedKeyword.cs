d5494f11e79c7f6c549ef05c4dc918b7a37cd750
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-04 09:00:10 +0530

    Checked and Unchecked Keyword

---
+++ Maersk.Practice.CSharp.Console/CheckedUncheckedKeyword.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basics
+{
+    public class CheckedUncheckedKeyword
+    {
+        public static void Main(string[] args)
+        {
+            int max=int.MaxValue;
+            Console.WriteLine("Max value of integer: "+max);
+            int a = max; 
+            int b=max;
+            int c = a + b;
+            Console.WriteLine("Max value without checked and unchecked keyword: "+c);
+
+            int e = unchecked(a + c);
+            Console.WriteLine("Max value with unchecked keyword: "+e);
+
+            try
+            {
+                int d = checked(a + b);
+                Console.WriteLine("Max value with checked keyword: " + d);
+            }
+            catch(Exception f)
+            {
+                Console.WriteLine(f);
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
