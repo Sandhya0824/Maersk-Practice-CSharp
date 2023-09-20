04a585de9394882eb410b1ef6864cb9b79a3e6a8
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-21 00:10:16 +0530

    List Example - Collections

---
+++ Maersk.Practice.CSharp.Console/Collections/ListExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basics.Collections
+{
+    public class ListExample
+    {
+        public static void Main(string[] args)
+        {
+            var names = new List<string>();
+            names.Add("Aakriti Kumari");
+            names.Add("Manaswini Ray");
+            names.Add("Sandhya Rani");
+
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+
+            var internsName = new List<string>() { "Aakriti", "Manaswini", "Sandhya"};
+            foreach(var interns in internsName)
+            {
+                Console.WriteLine(interns);
+            }
+        }
+    }
+}
