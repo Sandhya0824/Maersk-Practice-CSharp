404cbca9970982d7bec326a90da4a0ac21537e7e
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-22 14:44:07 +0530

    List<T> Example

--- Maersk.Practice.CSharp.Console/Collections/ListExample.cs
+++ Maersk.Practice.CSharp.Console/Collections/ListExample.cs
@@ -10,6 +10,23 @@ namespace C__Basics.Collections
     {
         public static void Main(string[] args)
         {
+            List<int> firstList = new List<int>();
+            Console.WriteLine("Display the number of elements of List<T>: "+firstList.Count);
+
+            firstList.Add(1);
+            firstList.Add(2);
+            firstList.Add(3);
+            firstList.Add(4);
+            Console.WriteLine("Capacity Is: " + firstList.Capacity);
+            Console.WriteLine("Count Is: " + firstList.Count);
+
+            firstList.Add(5);
+            firstList.Add(6);
+            // Printing the Capacity of firstlist. It will give output 8 as internally list is resized
+            // (the current implementation detail will double it's backing store)
+            Console.WriteLine("Capacity Is: " + firstList.Capacity);
+            Console.WriteLine("Count Is: " + firstList.Count);
+
             var names = new List<string>();
             names.Add("Aakriti Kumari");
             names.Add("Manaswini Ray");
