167f9647b12a50056fe1ef381d20f76b7f2ab306
Sandhya0824 <sandhyaa246789@gmail.com>
2023-09-27 04:57:42 +0530

    till Delegates

--- Maersk.Practice.CSharp.ConsoleUI/Generics/GenericsBasic.cs
+++ Maersk.Practice.CSharp.ConsoleUI/Generics/GenericsBasic.cs
@@ -8,5 +8,18 @@ namespace Maersk.Practice.CSharp.ConsoleUI.Generics
 {
     internal class GenericsBasic
     {
+        public void Show<T>(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void Main(string[] args)
+        {
+            GenericsBasic gen = new GenericsBasic();
+            gen.Show("This is generic method");
+            gen.Show(101);
+            gen.Show('I');
+            gen.Show(true);
+        }
     }
 }
