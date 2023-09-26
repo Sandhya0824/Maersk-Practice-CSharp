2a8b5edf53ff47b163e4514c8e1bc165cc2f5767
Sandhya0824 <sandhyaa246789@gmail.com>
2023-09-27 05:00:48 +0530

    till Delegates

--- Maersk.Practice.CSharp.ConsoleUI/Delegates/Calculator.cs
+++ Maersk.Practice.CSharp.ConsoleUI/Delegates/Calculator.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Maersk.Practice.CSharp.ConsoleUI.Delegates
 {
-    internal class Calculator
+    public class Calculator
     {
+        public delegate double BinaryOperation(double x, double y);
+
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            if (y != 0)
+            {
+                return x / y;
+            }
+            else
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+        }
+
+        public double PerformOperation(double x, double y, BinaryOperation operation)
+        {
+            return operation(x, y);
+        }
+
     }
 }
--- Maersk.Practice.CSharp.ConsoleUI/Delegates/Program.cs
+++ Maersk.Practice.CSharp.ConsoleUI/Delegates/Program.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Maersk.Practice.CSharp.ConsoleUI.Delegates
 {
-    internal class Program
+    public class Program
     {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            double num1 = 10.0;
+            double num2 = 5.0;
+
+            double result1 = calculator.PerformOperation(num1, num2, calculator.Add);
+            Console.WriteLine($"Addition result: {result1}");
+
+            double result2 = calculator.PerformOperation(num1, num2, calculator.Subtract);
+            Console.WriteLine($"Subtraction result: {result2}");
+
+            double result3 = calculator.PerformOperation(num1, num2, calculator.Multiply);
+            Console.WriteLine($"Multiplication result: {result3}");
+
+            double result4 = calculator.PerformOperation(num1, num2, calculator.Divide);
+            Console.WriteLine($"Division result: {result4}");
+        }
     }
 }
