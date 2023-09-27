86f22ce60fb27f3d5b269f23693ed6e340ef241b
Sandhya0824 <sandhyaa246789@gmail.com>
2023-09-28 02:42:08 +0530

    multithreading

---
+++ Maersk.Practice.CSharp.ConsoleUI/Multithreading/MainThreadExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maersk.Practice.CSharp.ConsoleUI.Multithreading
+{
+    internal class MainThreadExample
+    {
+        static void Method1()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method1 :" + i);
+            }
+        }
+        static void Method2()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method2 :" + i);
+                if (i == 2)
+                {
+                    Console.WriteLine("Performing the Database/IO Operation Started");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Performing the Database/IO Operation Completed");
+                }
+            }
+        }
+        static void Method3()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method3 :" + i);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+            Method3();
+        }
+    }
+}
+}
---
+++ Maersk.Practice.CSharp.ConsoleUI/Multithreading/ThreadClassExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maersk.Practice.CSharp.ConsoleUI.Multithreading
+{
+    internal class ThreadClassExample
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = Thread.CurrentThread;
+            thread.Name = "Main Thread";
+            Console.WriteLine("Name given to the thread :" + thread.Name);
+            Console.WriteLine("Name of the thread that is currently executing :" + Thread.CurrentThread.Name);
+            Console.WriteLine(thread.IsAlive);
+            Console.WriteLine(thread.ThreadState);
+            Console.WriteLine(thread.ManagedThreadId);
+            //Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine(thread.IsBackground);
+            //Console.WriteLine(Thread.CurrentThread.IsBackground);
+            Console.WriteLine(thread.Priority);
+        }
+    }
+}
